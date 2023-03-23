d6c3d9e170b6c071572d2a00a665d94744db2b65
TestingPrac <rohithkarani123@gmail.com>
2023-03-23 21:39:05 +0530

    my commit

---
+++ src/browser/comment/Demo.java
@@ -0,0 +1,65 @@
+//package browser.comment;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.Cookie;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import java.util.Set;
+//import java.util.concurrent.TimeUnit;
+//
+//public class Demo {
+//
+//
+//    public static void main(String[] args) throws Exception {
+//        System.setProperty("webdriver.chrome.driver","C:\\Users\\a842074\\Downloads\\conductor-master\\Publin\\Driver\\chromedriver.exe");
+//            WebDriver driver=new ChromeDriver();
+////        driver.get("https://demoqa.com/dynamic-properties");
+////        String source = driver.getPageSource();
+////        System.out.println(source);
+////
+////String tille = driver.getTitle();
+////        System.out.println(tille);
+////    driver.close();
+////        driver.switchTo().frame(1);
+//
+//        driver.get("https://nxtgenaiacademy.com/multiplewindows/");
+//
+//        driver.navigate().to("https://demoqa.com/dynamic-properties");
+////        driver.findElement(By.xpath("//button[text()=\"New Browser Tab\"]")).click();
+////        driver.navigate().back();
+////        driver.close();
+//
+////        driver.quit();
+//
+//
+////        driver.get("https://demoqa.com/dynamic-properties");
+//
+////        driver.manage().deleteAllCookies();
+////        Set<Cookie> cookie = driver.manage().getCookies();
+////driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+////        System.out.println(cookie);
+////        Thread.sleep(3000);
+////        driver.manage().window().maximize();
+////        driver.get("https://demoqa.com/dynamic-properties");
+////        String title = driver.getTitle();
+////        System.out.println(title);
+////        String url1 = driver.getCurrentUrl();
+////        System.out.println(url1);
+////       String source = driver.getPageSource();
+////        System.out.println(source);
+////
+////        driver.close();
+////        driver.quit();
+////        driver.get("https://demoqa.com/dynamic-properties");
+//////        driver.get("https://demo.automationtesting.in/FileUpload.html");
+////
+////        driver.navigate().to("https://demoqa.com/dynamic-properties");
+////        driver.navigate().to("https://demo.automationtesting.in/FileUpload.html");
+////        driver.navigate().back();
+////        driver.navigate().refresh();
+//
+//
+//
+//    }
+//}
---
+++ src/browser/comment/Main.java
@@ -0,0 +1,44 @@
+//package browser.comment;
+//
+//import org.openqa.selenium.Point;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver","C:\\Users\\a842074\\Downloads\\conductor-master\\Publin\\Driver\\chromedriver.exe");
+//
+//        WebDriver driver=new ChromeDriver();
+//       driver.manage().window().maximize();
+//        driver.get("https://demoqa.com/dynamic-properties");
+//        String tiltle =driver.getTitle();
+//        System.out.println(tiltle);
+//       driver.quit();
+////
+////        driver.get("https://demoqa.com/dynamic-properties");
+////
+//
+//
+
+
+
+
+
+//        driver.manage().window().maximize();
+//      driver.get("https://stqatools.com/selenium-chrome-browser/");
+//      driver.manage().window().setPosition(new Point(100,-100));
+
+//        driver.navigate().to("https://stqatools.com/selenium-chrome-browser/");
+//        driver.navigate().to("http://www.google.com");
+//        driver.navigate().to("http://www.facebook.com");
+//        driver.navigate().to("http://www.india.com");
+//        driver.navigate().back();
+////        driver.navigate().forward();
+////        driver.navigate().refresh();
+//
+//        String a =driver.getTitle();
+//        System.out.println(a);
+//        System.out.println(driver.getCurrentUrl());
+//    }
+//}
